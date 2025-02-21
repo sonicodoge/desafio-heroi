f8b4d4dee91240b97d0f3bfb2adf3188addb9fa7
Ronan <143884519+sonicodoge@users.noreply.github.com>
2025-02-21 18:53:18 -0300

    Update heroi.js
    desafio da dio  nivel heroi.

--- heroi.js
+++ heroi.js
@@ -1,1 +1,18 @@
-git init
+const experiencia = 1000;
+const nome = "irineu"; // Definindo corretamente o nome como string
+
+if (experiencia <= 1000) {
+    console.log("ferro");
+} else if (experiencia >= 2000 && experiencia <= 5000) {
+    console.log("ouro");
+} else if (experiencia >= 7001 && experiencia <= 7000) {
+    console.log("Platina");
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+    console.log("Ascendente");
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+    console.log("Imortal");
+} else if (experiencia >= 10000) {
+    console.log("Radiante");
+}
+
+console.log(`O herói tem o nome ${nome} e está no nível ${experiencia}`);
